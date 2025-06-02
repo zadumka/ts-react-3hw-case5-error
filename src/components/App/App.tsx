fc0777d7c30687d9ce7fb86917df222dfbafb337
zadumka <90329284+zadumka@users.noreply.github.com>
2025-06-02 18:28:02 +0300

    Update App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,74 +1,71 @@
 import css from "./App.module.css";More actions
-// import axios from "axios";
-import type { Movie } from "../../types/movie";
-import { fetchMovies } from "../../services/movieService";
-import { useEffect, useState } from "react";
-
 import toast, { Toaster } from "react-hot-toast";
-
-import ErrorMessage from "../ErrorMessage/ErrorMessage";
-import Loader from "../Loader/Loader";
-import MovieGrid from "../MovieGrid/MovieGrid";
-import MovieModal from "../MovieModal/MovieModal";
+import { fetchMovies } from "../../services/movieService";
 import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import type { Movie } from "../../types/movie";
+import { useState } from "react";
+import MovieModal from "../MovieModal/MovieModal";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
 export default function App() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
- 
-  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoad, setIsLoad] = useState(false);
+  
+  const [isError, setIsError] = useState(false);
 
-  const handleSearch = async (query: string) => {
-    const trimmedQuery = query.trim();
-    if (!trimmedQuery) {
-      toast.error("Please enter a search query.");
-      return;
-    }
-
-    setLoading(true);
-    setError(false);
-    setMovies([]);
-    try {
-      const fetchedMovies = await fetchMovies(trimmedQuery);
-      if (fetchedMovies.length === 0) {
-        toast.error("No movies found for your request.");
-      }
-      setMovies(fetchedMovies);
-    } catch (err) {
-      setError(true);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const handleSelect = (movie: Movie) => {
-    setSelectedMovie(movie);
-  };
-
-  const handleCloseModal = () => {
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => {
+    setIsModalOpen(false);
     setSelectedMovie(null);
   };
-  useEffect(() => {
-    document.body.style.overflow = selectedMovie ? "hidden" : "auto";
+  const [movies, setMovies] = useState<Movie[]>([]);
 
-    return () => {
-      document.body.style.overflow = "auto";
-    };
-  }, [selectedMovie]);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  const handleSelectMovie = (movieId: number) => {
+    const result = movies.find((movie) => movie.id === movieId);
+    if (result) {
+      setSelectedMovie(result);
+    }
+    openModal();
+  };
+
+  
+  const handleSearch = async (query: string) => {
+    setMovies([]);
+    setIsLoad(true);
+   
+    setIsError(false);
+    try {
+      const data = await fetchMovies({ query });
+      if (data.length == 0) {
+        throw new Error("No movies found for your request.");
+      }
+      setMovies([...data]);
+      
+    } catch (error) {
+      toast.error(`${error}`);
+      setIsError(true);
+    } finally {
+      setIsLoad(false);
+     
+    }
+  };
 
   return (
     <div className={css.app}>
-      <SearchBar onSubmit={handleSearch} />
-      {loading && <Loader />}
-      {error && <ErrorMessage />}
-      {!loading && !error && movies.length > 0 && (
-        <MovieGrid movies={movies} onSelect={handleSelect} />
-      )}
-      {selectedMovie && (
-        <MovieModal movie={selectedMovie} onClose={handleCloseModal} />
-      )}
       <Toaster position="top-center" />
+      <SearchBar onSubmit={handleSearch} />
+      {movies.length > 0 && (
+        <MovieGrid movies={movies} onSelect={handleSelectMovie} />
+      )}
+      {isLoad && <Loader />}
+     
+      {isError && <ErrorMessage />}
+      {isModalOpen && <MovieModal movie={selectedMovie} onClose={closeModal} />}
+     
     </div>
   );
 }
