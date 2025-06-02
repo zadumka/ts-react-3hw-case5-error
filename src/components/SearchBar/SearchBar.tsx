28e4a38d08254b45c9e0561ead795e696453859c
zadumka <90329284+zadumka@users.noreply.github.com>
2025-06-02 17:36:48 +0300

    Update SearchBar.tsx

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,45 +1,57 @@
-import toast from 'react-hot-toast';
-import styles from './SearchBar.module.css';
+// Компонент SearchBar приймає один пропс onSubmit – функцію для передачі значення інпуту під час сабміту форми.More actions
+import styles from "./SearchBar.module.css";
+import toast from "react-hot-toast";
 
 interface SearchBarProps {
-    onSubmit: (searchQuery: number) => void;
+  onSubmit: (query: string) => void;
 }
 
-export default function SearchBar({ onSubmit }: SearchBarProps) {
-    const handleSubmit = (formData: FormData) => {
-        const query = formData.get('query') as string;
-        if (query === '') {
-            toast.error('Please enter your search query.');
-            return;
-        }
-        onSubmit(Number(query));
-    };
+function SearchBar({ onSubmit }: SearchBarProps) {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-    return (
-        <header className={styles.header}>
-            <div className={styles.container}>
-                <a
-                    className={styles.link}
-                    href=""https://www.themoviedb.org/""
-                    target=""_blank""
-                    rel=""noopener noreferrer""
-                >
-                    Powered by TMDB
-                </a>
-                <form className={styles.form} action={handleSubmit}>
-                    <input
-                        className={styles.input}
-                        type=""text""
-                        name=""query""
-                        autoComplete=""off""
-                        placeholder=""Search movies...""
-                        autoFocus
-                    />
-                    <button className={styles.button} type=""submit"">
-                        Search
-                    </button>
-                </form>
-            </div>
-        </header>
-    );
-}
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+  
+    const query = (formData.get("query") as string).trim();
+
+    if (!query) {
+      toast.error("Please enter your search query.");
+      return;
+    }
+
+    onSubmit(query);
+    form.reset();
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <a
+          className={styles.link}
+          href="https://www.themoviedb.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by TMDB
+        </a>
+        <form className={styles.form} onSubmit={handleSubmit}>
+        
+          <input
+            className={styles.input}
+            type="text"
+            name="query"
+            autoComplete="off"
+            placeholder="Search movies..."
+            autoFocus
+          />
+          <button className={styles.button} type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    </header>
+  );
+}More actions
+
+export default SearchBar;
