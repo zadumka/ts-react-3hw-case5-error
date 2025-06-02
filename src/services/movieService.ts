0f145ca4cc26f94516a86bd6cd7264f3f8f820b2
zadumka <90329284+zadumka@users.noreply.github.com>
2025-06-02 16:53:22 +0300

    Update movieService.ts

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,14 +1,15 @@
-export const fetchMovies = async (query: string): Promise<Movie[]> => {
-    const response = await axios.get<TMDBResponse>(
-        'https://api.themoviedb.org/3/search/movie',
-        {
-            params: {
-                query,
-            },
-            headers: {
-                Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
-            },
-        },
-    );
-    return response.data.results;
+const params = {Add commentMore actions
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+    accept: "application / json",
+    accept: "application/json",
+  },
+};
+
+export const getMovies = async (query: string) => {
+  const res = await axios.get<GetMoviesRes>(
+    `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`,
+    params
+  );
+  return res.data.results;
 };
